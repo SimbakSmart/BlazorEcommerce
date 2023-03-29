370955eaaa84a82b113b3598540b6b0a00d345d0
SimbakSmart <simbak.smart@gmail.com>
2023-03-29 16:59:17 -0600

    04 - Product Details, Categories & Seeding More Products.

--- BlazorEcommerce/Client/Services/ProductServices/IProductService.cs
+++ BlazorEcommerce/Client/Services/ProductServices/IProductService.cs
@@ -6,5 +6,7 @@ namespace BlazorEcommerce.Client.Services.ProductServices
     {
         List<Product> Products { get; set; }
         Task GetProducts();
+
+        Task<ServiceResponse<Product>> GetProduct(int productId);
     }
 }
--- BlazorEcommerce/Client/Services/ProductServices/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductServices/ProductService.cs
@@ -20,5 +20,11 @@ namespace BlazorEcommerce.Client.Services.ProductServices
             if (result != null && result.Data != null)
                 Products = result.Data;
         }
+
+        public async Task<ServiceResponse<Product>> GetProduct(int productId)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{productId}");
+            return result;
+        }
     }
 }
