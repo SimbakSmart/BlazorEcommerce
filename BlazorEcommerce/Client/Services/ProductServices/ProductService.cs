4b39dd4c91c1b9d8fed34b61133c9b1e82c13e0f
SimbakSmart <simbak.smart@gmail.com>
2023-03-31 11:43:01 -0600

    08 - Implemented the Product Search Feature.

--- BlazorEcommerce/Client/Services/ProductServices/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductServices/ProductService.cs
@@ -12,6 +12,7 @@ namespace BlazorEcommerce.Client.Services.ProductServices
         }
 
         public List<Product> Products { get; set; } = new List<Product>();
+        public string Message { get; set; } = "Loading products...";
 
         public event Action ProductsChanged;
 
@@ -30,6 +31,23 @@ namespace BlazorEcommerce.Client.Services.ProductServices
                 Products = result.Data;
 
             ProductsChanged.Invoke();
+        }
+
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http
+                .GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http
+                 .GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+            if (result != null && result.Data != null)
+                Products = result.Data;
+            if (Products.Count == 0) Message = "No products found.";
+            ProductsChanged?.Invoke();
         }
     }
 }
