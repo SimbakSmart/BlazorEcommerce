bad283bdc7c3c768587087c9b42f57b5017adc08
SimbakSmart <simbak.smart@gmail.com>
2023-03-31 14:54:42 -0600

    11 - Pagination for Product Search.

--- BlazorEcommerce/Client/Services/ProductServices/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductServices/ProductService.cs
@@ -13,6 +13,9 @@ namespace BlazorEcommerce.Client.Services.ProductServices
 
         public List<Product> Products { get; set; } = new List<Product>();
         public string Message { get; set; } = "Loading products...";
+        public int CurrentPage { get; set; } = 1;
+        public int PageCount { get; set; } = 0;
+        public string LastSearchText { get; set; } = string.Empty;
 
         public event Action ProductsChanged;
 
@@ -25,10 +28,16 @@ namespace BlazorEcommerce.Client.Services.ProductServices
         public async Task GetProducts(string? categoryUrl = null)
         {
             var result = categoryUrl == null ?
-                 await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product/featured") :
+                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product/featured") :
                 await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
             if (result != null && result.Data != null)
                 Products = result.Data;
+
+            CurrentPage = 1;
+            PageCount = 0;
+
+            if (Products.Count == 0)
+                Message = "No products found";
 
             ProductsChanged.Invoke();
         }
@@ -40,12 +49,17 @@ namespace BlazorEcommerce.Client.Services.ProductServices
             return result.Data;
         }
 
-        public async Task SearchProducts(string searchText)
+        public async Task SearchProducts(string searchText, int page)
         {
+            LastSearchText = searchText;
             var result = await _http
-                 .GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+                 .GetFromJsonAsync<ServiceResponse<ProductSearchResult>>($"api/product/search/{searchText}/{page}");
             if (result != null && result.Data != null)
-                Products = result.Data;
+            {
+                Products = result.Data.Products;
+                CurrentPage = result.Data.CurrentPage;
+                PageCount = result.Data.Pages;
+            }
             if (Products.Count == 0) Message = "No products found.";
             ProductsChanged?.Invoke();
         }
