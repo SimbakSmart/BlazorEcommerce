79eb65d140f6e2127ca8d27e1accf05547d1950a
SimbakSmart <simbak.smart@gmail.com>
2023-04-03 16:54:23 -0600

    20 - Database Cart Implementations (incl. all CRUD operations for Cart Ite…

--- BlazorEcommerce/Shared/CartItem.cs
+++ BlazorEcommerce/Shared/CartItem.cs
@@ -4,7 +4,7 @@ namespace BlazorEcommerce.Shared
 {
     public class CartItem
     {
-
+        public int UserId { get; set; }
         public int ProductId { get; set; }
         public int ProductTypeId { get; set; }
         public int Quantity { get; set; } = 1;
